c5789207b92ca8dd982410d8390cf24f4f846838
yangqi <yangqi@qudian.com>
2018-07-04 23:41:11 +0800

    upd: 升级生成模板命令

--- gulpfile.js
+++ gulpfile.js
@@ -82,32 +82,80 @@ gulp.task('dev', gulp.series('default', 'watch'));
 
 /**
  * auto 自动创建page or template or component
- *  -s 源目录（默认为_template） -d 目的目录（即要生成的目录名) -t 生成的类型 p page t template c component  （默认 p)
+ *  -s 源目录（默认为_template)
  * @example
- * gulp auto -s=xx -d=xxx -t=p
- * gulp auto -s=_template -d=mypage
- * gulp auto  -d=testt -t=t
+ *   gulp auto -p mypage           创建名称为mypage的page文件
+ *   gulp auto -t mytpl            创建名称为mytpl的template文件
+ *   gulp auto -c mycomponent      创建名称为mycomponent的component文件
+ *   gulp auto -s index -p mypage  创建名称为mypage的page文件
  */
-const auto = () => {
-  const argv = require('yargs').argv;
-  let source = argv.s || '_template';
-  let destination = argv.d;
-  let type = argv.t || 'p';
-  let typeEnum = {
-    p: 'pages',
-    t: 'template',
-    c: 'components'
-  };
-  let root = path.join(__dirname, 'src', typeEnum[type]);
+const auto = (done) => {
+  const yargs = require('yargs')
+    .example('gulp auto -p mypage', '创建名为mypage的page文件')
+    .example('gulp auto -t mytpl', '创建名为mytpl的template文件')
+    .example('gulp auto -c mycomponent', '创建名为mycomponent的component文件')
+    .example('gulp auto -s index -p mypage', '复制pages/index中的文件创建名称为mypage的页面')
+    .option({
+      s: {
+        alias: 'src',
+        default: '_template',
+        describe: 'copy的模板',
+        type: 'string'
+      },
+      p: {
+        alias: 'page',
+        describe: '生成的page名称',
+        conflicts: ['t', 'c'],
+        type: 'string'
+      },
+      t: {
+        alias: 'template',
+        describe: '生成的template名称',
+        type: 'string',
+        conflicts: ['c']
+      },
+      c: {
+        alias: 'component',
+        describe: '生成的component名称',
+        type: 'string'
+      },
+      version: { hidden: true },
+      help: { hidden: true }
+    })
+    .help('msg');
 
-  return gulp
-    .src(path.join(root, source, `*.*`))
-    .pipe(
-      rename({
-        dirname: destination,
-        basename: destination
-      })
-    )
+    const argv = yargs.argv;
+
+    const source = argv.s;
+    const typeEnum = {
+      p: 'pages',
+      t: 'templates',
+      c: 'components'
+    };
+    let hasParams = false;
+    let name, type;
+    for(let key in typeEnum) {
+      hasParams = hasParams || !!argv[key];
+      if (argv[key]) {
+        name = argv[key];
+        type = typeEnum[key];
+      }
+    }
+
+    if(!hasParams) {
+      done();
+      yargs.parse(['--msg']);
+    }
+
+    const root = path.join(__dirname, 'src', type);
+    return gulp
+      .src(path.join(root, source, `*.*`))
+      .pipe(
+        rename({
+          dirname: name,
+          basename: name
+        })
+      )
     .pipe(gulp.dest(path.join(root)));
 };
 gulp.task(auto);
